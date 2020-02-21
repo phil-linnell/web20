0d00e95a4c4e60fa7e875a5eee68d3a1a1d157f2
Phil <phil@ustwo.com>
2020-02-21 10:36:25 +0000

    Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
+
 module.exports = {
   plugins: [
     `gatsby-plugin-preact`,
-    `gatsby-plugin-linaria`
+    `gatsby-plugin-linaria`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ""
+      }
+    }
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
 import React from "react";
+import { graphql } from "gatsby";
 import { css } from 'linaria';
 
 import Layout from "../components/layout";
 
 const cssWrapper = css`
-  p {
-    color: rebeccapurple
+  h2 {
+    color: #666
   }
 `;
 
-export default () => (
-  <Layout>
-    <div className={cssWrapper}>
-      <h1>Web 20</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-      <h2>Heading 2</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </div>
-  </Layout>
-);
+export default ({ data }) => {
+  const pages = data.allContentfulPage.edges.map(x => x.node);
+
+  return (
+    <Layout>
+      <div className={cssWrapper}>
+        <h1>Web 20</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <h2>Heading 2</h2>
+        {pages.map(page => (
+          <div>{page.name}</div>
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query {
+    allContentfulPage {
+      edges {
+        node {
+          name
+          description
+        }
+      }
+    }
+  }
+`;
